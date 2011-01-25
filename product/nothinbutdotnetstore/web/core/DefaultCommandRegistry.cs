08d9d106f4005c0979a98ed54a26d96cfee49db7
unknown <cduncan@.invisioninc.local>
2011-01-25 13:44:55 -0500

    Pushing new changes

--- product/nothinbutdotnetstore/web/core/DefaultCommandRegistry.cs
+++ product/nothinbutdotnetstore/web/core/DefaultCommandRegistry.cs
@@ -5,14 +5,24 @@ namespace nothinbutdotnetstore.web.core
 {
     public class DefaultCommandRegistry : CommandRegistry
     {
+        IEnumerable<RequestCommand> registry;
         public DefaultCommandRegistry(IEnumerable<RequestCommand> all_commands)
         {
-            
+            this.registry = all_commands;
         }
 
         public RequestCommand get_command_that_can_run(Request request)
         {
-            throw new NotImplementedException();
+            RequestCommand retval = null;
+            foreach (var request_command in registry)
+            {
+                if (request_command.can_process(request))
+                {
+                    retval = request_command;
+                }
+            }
+
+            return retval;
         }
     }
 }
