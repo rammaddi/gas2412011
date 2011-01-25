aad3a71dd4905f7ead3566118b7ffd70180603b8
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-01-25 13:53:58 -0500

    Refactored the test

--- product/nothinbutdotnetstore.specs/web/CommandRegistrySpecs.cs
+++ product/nothinbutdotnetstore.specs/web/CommandRegistrySpecs.cs
@@ -1,19 +1,27 @@
- using System.Collections.Generic;
- using System.Linq;
- using Machine.Specifications;
- using Machine.Specifications.DevelopWithPassion.Rhino;
- using nothinbutdotnetstore.web.core;
- using Rhino.Mocks.Impl;
- using Rhino.Mocks;
+using System.Collections.Generic;
+using System.Linq;
+using Machine.Specifications;
+using Machine.Specifications.DevelopWithPassion.Rhino;
+using nothinbutdotnetstore.web.core;
+using Rhino.Mocks;
 
 namespace nothinbutdotnetstore.specs.web
-{   
+{
     public class CommandRegistrySpecs
     {
         public abstract class concern : Observes<CommandRegistry,
                                             DefaultCommandRegistry>
         {
-        
+            Establish c = () =>
+            {
+                request = an<Request>();
+                all_commands = Enumerable.Range(1, 100).Select(x => an<RequestCommand>()).ToList();
+                provide_a_basic_sut_constructor_argument<IEnumerable<RequestCommand>>(all_commands);
+            };
+
+            public static Request request;
+            public static RequestCommand result;
+            protected static List<RequestCommand> all_commands;
         }
 
         [Subject(typeof(DefaultCommandRegistry))]
@@ -21,53 +29,28 @@ namespace nothinbutdotnetstore.specs.web
         {
             Establish c = () =>
             {
-                request = an<Request>();
-                all_commands = Enumerable.Range(1,100).Select(x => an<RequestCommand>()).ToList();
                 the_command_that_can_handle_the_request = an<RequestCommand>();
                 all_commands.Add(the_command_that_can_handle_the_request);
-
                 the_command_that_can_handle_the_request.Stub(x => x.can_process(request)).Return(true);
-
-                provide_a_basic_sut_constructor_argument<IEnumerable<RequestCommand>>(all_commands);
-
             };
 
             Because b = () =>
                 result = sut.get_command_that_can_run(request);
 
-
             It should_return_the_command_that_can_handle_the_request = () =>
                 result.ShouldEqual(the_command_that_can_handle_the_request);
 
-
-            static RequestCommand result;
             static RequestCommand the_command_that_can_handle_the_request;
-            static Request request;
-            static List<RequestCommand> all_commands;
-            static IEnumerable<RequestCommand> command_iterator;
         }
 
         [Subject(typeof(DefaultCommandRegistry))]
         public class when_finding_a_command_that_can_handle_a_request_and_it_does_not_have_the_command : concern
         {
-            Establish c = () =>
-            {
-                request = an<Request>();
-                all_commands = Enumerable.Range(1,100).Select(x => an<RequestCommand>()).ToList();
-                provide_a_basic_sut_constructor_argument<IEnumerable<RequestCommand>>(all_commands);
-            };
-
             Because b = () =>
                 result = sut.get_command_that_can_run(request);
 
-
             It should_return_a_missing_command = () =>
                 result.ShouldBeAn<MissingRequestCommand>();
-
-
-            static RequestCommand result;
-            static Request request;
-            static List<RequestCommand> all_commands;
         }
     }
 }
