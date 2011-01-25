dcb4c26dd076d549767b44e076316c21beb9b191
cpassante <cpassante@invisioninc.com>
2011-01-25 13:16:00 -0500

    Pushing new changes

--- product/nothinbutdotnetstore/web/core/DefaultFrontController.cs
+++ product/nothinbutdotnetstore/web/core/DefaultFrontController.cs
@@ -4,9 +4,17 @@ namespace nothinbutdotnetstore.web.core
 {
     public class DefaultFrontController : FrontController
     {
+        CommandRegistry command_registry;
+
+        public DefaultFrontController(CommandRegistry command_registry)
+        {
+            this.command_registry = command_registry;
+        }
+
         public void process(Request request)
         {
-            throw new NotImplementedException();
+            var command_that_can_run = command_registry.get_command_that_can_run(request);
+            command_that_can_run.run(request);
         }
     }
 }
