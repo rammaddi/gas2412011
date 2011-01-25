fe361a2269f88a8d15e44105d640093cb8bad13c
unknown <vraju@.invisioninc.local>
2011-01-25 13:50:21 -0500

    Committing

--- product/nothinbutdotnetstore/web/core/DefaultFrontController.cs
+++ product/nothinbutdotnetstore/web/core/DefaultFrontController.cs
@@ -4,9 +4,15 @@ namespace nothinbutdotnetstore.web.core
 {
     public class DefaultFrontController : FrontController
     {
+        CommandRegistry registry;
+
+        public DefaultFrontController(CommandRegistry registry)
+        {
+            this.registry = registry;
+        }
         public void process(Request request)
         {
-            throw new NotImplementedException();
+            registry.get_command_that_can_run(request).run(request);
         }
     }
 }
