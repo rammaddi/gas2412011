7952e7fd241846322a01520399a00fa652bedaf1
unknown <vraju@.invisioninc.local>
2011-01-25 12:02:17 -0500

    Pushing new changes

--- product/nothinbutdotnetstore/web/core/RawHandler.cs
+++ product/nothinbutdotnetstore/web/core/RawHandler.cs
@@ -5,14 +5,27 @@ namespace nothinbutdotnetstore.web.core
 {
     public class RawHandler : IHttpHandler
     {
+        FrontController controller;
+        RequestFactory factory;
+
+        public RawHandler(FrontController controller, RequestFactory factory)
+        {
+            this.controller = controller;
+            this.factory = factory;
+        }
+        
+
         public void ProcessRequest(HttpContext context)
         {
-            throw new NotImplementedException();
+            
+            this.controller.process(factory.create(context));
         }
 
         public bool IsReusable
         {
             get { throw new NotImplementedException(); }
         }
+
+        
     }
 }
