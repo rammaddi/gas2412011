9cc8a6d02f8312983a98793385aa7b2faed36a83
Bradley Dean <Bradley@TrueGeek.com>
2011-01-26 19:02:53 -0500

    Pushing new changes

--- product/nothinbutdotnetstore.specs/web/core/LinkCommandSpecs.cs
+++ product/nothinbutdotnetstore.specs/web/core/LinkCommandSpecs.cs
@@ -25,6 +25,12 @@ namespace nothinbutdotnetstore.specs.web.core
 			It should_return_a_valid_url_for_the_department = () =>
 				LinkCommand.GetName<ViewDepartmentsInDepartment>(department).ShouldEqual("ViewDepartmentsInDepartment.invision?Department=Test");
 
+			It should_return_a_url_for_products_in_department = () =>
+				LinkCommand.GetName<ViewProductsInDepartment>().ShouldEqual("ViewProductsInDepartment.invision");
+
+			It should_return_a_valid_url_for_no_department = () =>
+				LinkCommand.GetName<ViewDepartmentsInDepartment>(new Department()).ShouldEqual("ViewDepartmentsInDepartment.invision?Department=");
+
 			static Department department;
 				
 		}
